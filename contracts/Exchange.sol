4efe4f9d9908b1bce6530b68fe55d0e603a29086
Soheil Parsaei <106927616+Soheilprs@users.noreply.github.com>
2024-01-19 18:21:56 +0330

    Update Exchange.sol

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -1,1 +1,17 @@
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Exchange is ERC20 {
+    address public tokenAddress;
+
+    constructor(address token) ERC20("Codeyad LP Token", "CYDlpToken") {
+        require(token != address(0), "Token address passed is a null address");
+        tokenAddress = token;
+    }
+
+    function getReserve() public view returns (uint256) {
+        return ERC20(tokenAddress).balanceOf(address(this));
+    }
+}
 
